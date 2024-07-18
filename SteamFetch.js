60aff48d8d3bbe9c030bceae0a27ea1cb5c39978
sammylinkton <127231219+sammylinkton@users.noreply.github.com>
2024-07-18 03:13:14 -0400

    meow

--- SteamFetch.js
+++ SteamFetch.js
@@ -7,59 +7,57 @@ async function SteamFetch() {
     // Setting the PFP
 
     const pfp = document.getElementById("SteamPFP");
- //   console.log("Setting PFP...");
+    //   console.log("Setting PFP...");
     pfp.src = data.response.players[0].avatarfull;
- //   console.log("PFP Set!");
+    //   console.log("PFP Set!");
 
     // Getting the persona state, its just a status indicator like Discord.
-
-  //  console.log("Getting Persona State");
-   // console.log(data.response.players[0].personastate);
     if (data.response.players[0].personastate == 1) {
         pfp.style.borderColor = "Green";
+        document.getElementById("Name").style.color = "Green";
     } else if (data.response.players[0].personastate == 2) {
         pfp.style.borderColor = "Orange";
     } else if (data.response.players[0].personastate == 3) {
         pfp.style.borderColor = "Orange";
+        document.getElementById("Name").style.color = "Orange";
     } else {
-        pfp.style.borderColor = "DarkGrey";
+        pfp.style.borderColor = "#4f4f4f";
+        document.getElementById("Name").style.color = "#4f4f4f";
     }
 
     // Getting the persona username (thats just what your username is displayed to everyone as), and the users real name, if displayed.
 
     const name = document.getElementById("Name");
-  //  console.log("Profile username:");
-   // console.log(data.response.players[0].personaname);
+    //  console.log("Profile username:");
+    // console.log(data.response.players[0].personaname);
     const username = data.response.players[0].personaname;
-  //  console.log("Profile Real Name");
+    //  console.log("Profile Real Name");
     if (data.response.players[0].realname == undefined) {
-       // console.log("Real name not found, omitting it");
+        // console.log("Real name not found, omitting it");
         const realname = "";
         name.innerHTML = username;
     } else {
-   //     console.log("If it goes here with it being undefined, ill kill myself");
+        //     console.log("If it goes here with it being undefined, ill kill myself");
         const realname = '"' + data.response.players[0].realname + '"';
         name.innerHTML = "" + username + " " + realname;
     }
     // Checking if user is playing a game
     if (data.response.players[0].gameid == undefined) {
-    //    console.log("Not playing a game currently");
+        //    console.log("Not playing a game currently");
     } else {
         //getting game id
         const opa = document.getElementById("Controller").style.opacity;
         document.getElementById("Controller").style.opacity = 1;
         document.getElementById("GameImage").innerHTML = "Playing " + data.response.players[0].gameextrainfo;
     }
-    
+
     // listing all values gotten
-    
+
     const dataset = [player.avatarfull, player.personastate, player.personaname, player.gameid];
     console.log(dataset);
-    
-    
 }
 
 console.log("First Run");
 SteamFetch();
-console.log("Running Code in 10,000 ms");
-setInterval(SteamFetch,10000);
+console.log("Running Code in 5000 ms");
+setInterval(SteamFetch, 5000);
